fedc0edf60a4bc218ad06e2aa2fdcb3356766268
aslingguang <aslingguang@gmail.com>
2024-02-28 00:01:29 +0800

    完善批量包管理脚本

--- .config/zsh/script/package_installer.sh
+++ .config/zsh/script/package_installer.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+
 if [[ "$(id -u)" -ne 0 ]]; then
-    echo -e "\e[31m以非root用户运行。切换到root权限...\e[0m\n"
+    echo -e "\e[31m以非root用户运行。切换到root权限...\e[0m"
     exec sudo "$0" "$@"
 fi
 
 helpinfo="    package-installer使用方法
-    pi [options] [...] <input_file>
+    pi [options] [...] package1 package2 ... 
     eg: pi -i apt packages or pi -i packages
-    -s              批量检索软件包    [选项] [包管理器(可选)]
-    -i              批量安装软件包    [选项] [包管理器(可选)]
-    -ic             批量安装软件包(检查对应包命令是否存在)    [选项] [包管理器(可选)]
-    -r              批量卸载软件包    [选项] [包管理器(可选)]
-    -h, --help      帮助信息
-    input_file    软件包列表文件
+    -s                            检索软件包    
+    -i                            安装软件包    
+    -ic                           安装软件包(检查对应包命令是否存在)    
+    -f, --file <packages_file>     指定软件包列表文件   
+    -r                            卸载软件包    
+    -m <package_manager>          指定包管理器
+    -h, --help                    帮助信息
     
     软件包列表文件格式说明:
     #为注释符，每行一个软件包名，可选择在软件包后以空格隔开写入软件包的对应启动命令(eg: git git)。
     若包后有启动命令，可使用./package_install.sh -ic packages命令在安装软件时检查软件启动命令是否存在，若存在，则不会安装该包。"
-
 
 
 load_custom_package_manager()
@@ -64,8 +65,6 @@ load_custom_package_manager()
 	echo -e "\e[34m包管理器为 ${package_manager}\e[0m"
 }
 
-
-
 load_default_package_manager() 
 {
     # 使用默认包管理器
@@ -111,6 +110,7 @@ load_default_package_manager()
 	fi
 }
 
+
 get_package_name()
 {
     # 跳过以#开头的注释行和空行
@@ -151,54 +151,49 @@ get_package_name()
     
 }
 
-load_package_manager()
-{
-    if [[ -z "$3" && -f "$2" ]]; then
-        # 读取输入文件中的软件包名称
-        input_file="$2"
-        load_default_package_manager
-    elif [[ -n "$3" && -f "$3" ]]; then
-        # 读取输入文件中的软件包名称
-        input_file="$3"
-        package_manager="$2"
-        load_custom_package_manager 
-    else
-        echo "请输入正确的参数"
-        exit 1
-    fi
-}
-
-# 批量检索软件
+# 检索软件
 search_package()
 {
-    # 使用while循环逐行读取文件中的软件包名称
-    while IFS= read -r package || [[ -n "$package" ]]; do
+    # 获取正确的包名
+    get_package_name
 
-        # 获取正确的包名
-        get_package_name
-
-        if [[ $is_package == false ]]; then
-            continue
-        fi
-        
-        # 使用指定包管理器进行软件包搜索
-        result=$(eval "${search_command}")
-
-        # 检查搜索结果是否为空
-        if [[ -z "$result" ]]; then
-            error_search_results+="\e[31m软件包 $package_name 未找到\e[0m\n"
-        fi
-    done <"$input_file"
-    if [[ -z "$error_search_results" ]]; then
-        echo -e "\e[32m找到全部软件包\e[0m"
-    else
-        echo -e "$error_search_results"
+    if [[ $is_package == false ]]; then
+        return
     fi
+    
+    # 使用指定包管理器进行软件包搜索
+    search_result=$(eval "${search_command}")
+
+    # 检查搜索结果是否为空
+    if [[ -z "$search_result" ]]; then
+        error_results+="\e[31m软件包 $package_name 未找到\e[0m\n"
+    else
+        echo -e "\e[32m软件包 $package_name 已找到\e[0m"
+    fi
+
 }
+
 
 # 安装软件
 install_package()
 {
+    # 获取正确的包名
+    get_package_name
+
+    if [[ $is_package == false ]]; then
+        return
+    fi
+
+    # 检查对应软件包的启动命令是否已存在
+    if [[ "$check_command_option" == "install_package_with_check_command" ]]; then
+        if [[ -n "$start_conmand" ]]; then
+            if command -v $start_conmand &>/dev/null; then
+                echo -e "软件包 $package_name 的启动命令 $start_conmand 已存在\e[0m"
+                return
+            fi
+        fi
+    fi
+
     # 查询本地是否已安装软件包
     query_result=$(eval "${query_command}")
     if [[ -n "$query_result" ]]; then
@@ -210,130 +205,133 @@ install_package()
     search_result=$(eval "${search_command}")
     # 检查搜索结果是否为空
     if [[ -z "$search_result" ]]; then
-        error_search_results+="\n\e[31m软件包 $package_name 未找到\e[0m"
+        error_results+="\n\e[31m软件包 $package_name 未找到\e[0m"
         return
     fi
 
     # 安装软件包
-    install_result=$(eval "${install_command}")
+    install_result=$(eval "${install_command}" | grep -Po "error")
     if [[ -z "$install_result" ]]; then
         echo -e "\e[32m软件包 $package_name 安装成功\e[0m"
     else
-        error_install_results+="\n\e[31m软件包 $package_name 安装失败\e[0m\n"$install_result
+        error_results+="\n\e[31m软件包 $package_name 安装失败\e[0m\n"
     fi
 }
 
-# 批量安装软件
-install_package_without_check_command()
-{
-    # 使用while循环逐行读取文件中的软件包名称
-    while IFS= read -r package || [[ -n "$package" ]]; do
 
-        # 获取正确的包名
-        get_package_name
-
-        if [[ $is_package == false ]]; then
-            continue
-        fi
-
-        # 安装软件包
-        install_package
-
-    done <"$input_file"
-    if [[ -n "$error_search_results" ]]; then
-        echo -e "$error_search_results"
-    fi
-    if [[ -n "$error_install_results" ]]; then
-        echo -e "$error_install_results"
-    fi
-}
-
-# 批量安装软件(检查对应软件包的启动命令是否已存在)
-install_package_with_check_command()
-{
-    # 使用while循环逐行读取文件中的软件包名称
-    while IFS= read -r package || [[ -n "$package" ]]; do
-
-        # 获取正确的包名
-        get_package_name
-
-        if [[ $is_package == false ]]; then
-            continue
-        fi
-
-        if [[ -n "$start_conmand" ]]; then
-            if command -v $start_conmand &>/dev/null; then
-                echo -e "软件包 $package_name 的启动命令 $start_conmand 已存在\e[0m"
-                continue
-            fi
-        fi
-
-        # 安装软件包
-        install_package
-
-
-    done <"$input_file"
-    if [[ -n "$error_search_results" ]]; then
-        echo -e "$error_search_results"
-    fi
-    if [[ -n "$error_install_results" ]]; then
-        echo -e "$error_install_results"
-    fi   
-}
-
-# 批量卸载软件
+# 卸载软件
 uninstall_package()
 {
-    # 使用while循环逐行读取文件中的软件包名称
-    while IFS= read -r package || [[ -n "$package" ]]; do
 
-        # 获取正确的包名
-        get_package_name
+    # 获取正确的包名
+    get_package_name
 
-        if [[ $is_package == false ]]; then
-            continue
-        fi
-        
-        # 查询本地是否已安装软件包
-        query_result=$(eval "${query_command}")
-        if [[  -z "$query_result" ]]; then
-            echo -e "软件包 $package_name 未安装\e[0m"
-            continue
-        fi
+    if [[ $is_package == false ]]; then
+        continue
+    fi
+    
+    # 查询本地是否已安装软件包
+    query_result=$(eval "${query_command}")
+    if [[  -z "$query_result" ]]; then
+        echo -e "软件包 $package_name 未安装\e[0m"
+        continue
+    fi
 
-        # 卸载软件包
-        uninstall_result=$(eval "${uninstall_command}")
-        if [[ -z "$uninstall_result" ]]; then
-            echo -e "\e[32m软件包 $package_name 卸载成功\e[0m"
-        else
-            error_uninstall_results+="\n\e[31m软件包 $package_name 卸载失败\e[0m\n"$uninstall_result
-        fi
-
-    done <"$input_file"
-    if [[ -n "$error_uninstall_results" ]]; then
-        echo -e "$error_uninstall_results"
+    # 卸载软件包
+    uninstall_result=$(eval "${uninstall_command}")
+    if [[ -z "$uninstall_result" ]]; then
+        echo -e "\e[32m软件包 $package_name 卸载成功\e[0m"
+    else
+        error_results+="\n\e[31m软件包 $package_name 卸载失败\e[0m\n"
     fi
 }
 
 
+#-o或--options选项后面接可接受的短选项，如ab:c::，表示可接受的短选项为-a -b -c，其中-a选项不接参数，-b选项后必须接参数，-c选项的参数为可选的
+#-l或--long选项后面接可接受的长选项，用逗号分开，冒号的意义同短选项。
+#-n选项后接选项解析错误时提示的脚本名字
+ARGS=`getopt -o srhm:f:i:: --long help,file: -n "$0" -- "$@"`
+if [ $? != 0 ]; then
+    echo "参数错误..."
+    exit 1
+fi
 
-# 检查参数是否为空
-if [[ -z "$1" ]]; then
-	echo "请输入正确的参数"
-elif [[ "$1" == "-h"  ||  "$1" == "--help" ]]; then
-    echo "${helpinfo}"
-elif [[ $1 == "-s" ]]; then
-    load_package_manager  "$1" "$2" "$3"
-    search_package 
-elif [[ $1 == "-i" ]]; then
-    load_package_manager  "$1" "$2" "$3"
-    install_package_without_check_command 
-elif [[ $1 == "-ic" ]]; then
-    load_package_manager  "$1" "$2" "$3"
-    install_package_with_check_command 
-elif [[ $1 == "-r" ]]; then
-    load_package_manager  "$1" "$2" "$3"
-    uninstall_package 
+#echo $ARGS
+#将规范化后的命令行参数分配至位置参数（$1,$2,...)
+eval set -- "${ARGS}"
+
+while true
+do
+    case "$1" in
+        -h|--help) 
+            echo "${helpinfo}";
+            shift
+            ;;
+        -i) 
+            case "$2" in
+                "")
+                    check_command_option="install_package_without_check_command";
+                    package_manager_option="install_package";
+                    shift 2  
+                    ;;
+                *)
+                    if [[ "$2" == "c" ]];then
+                        check_command_option="install_package_with_check_command"
+                        package_manager_option="install_package";
+                    fi
+                    shift 2;
+                    ;;
+            esac
+            ;;
+        -s)
+            package_manager_option="search_package";
+            shift
+            ;;
+        -r)
+            package_manager_option="uninstall_package";
+            shift
+            ;;            
+        -m)
+            package_manager="$2";
+            shift 2
+            ;;
+        -f|--file)
+            package_file="$2";
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Internal error!"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -n $package_manager ]];then
+    load_custom_package_manager
 else
-	echo "请输入正确的参数"
+    load_default_package_manager
+fi
+
+if [[ -f $package_file ]];then
+    # 使用while循环逐行读取文件中的软件包名称
+    while IFS= read -r package || [[ -n "$package" ]]; do
+        eval "${package_manager_option}"
+    done <"$package_file"
+elif [[ -n $package ]];then
+    echo -e "\e[31m文件 $package_file 不存在\e[0m"
+fi
+
+#处理剩余的参数
+for arg in $@
+do
+    package="$arg"
+    eval "${package_manager_option}"
+done
+
+if [[ -n "$error_results" ]]; then
+    echo -e "$error_results"
 fi
